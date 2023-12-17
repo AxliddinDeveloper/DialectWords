ece0266b6a975201e5538bedf108d13fafcfec5a
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-12-17 16:47:24 +0500

    DATA: Create Word Model

---
+++ DialectWords/Models/Foundations/words/word.cs
@@ -0,0 +1,19 @@
+﻿namespace DialectWords.Models.Foundations.words
+{
+    public class word
+    {
+        public Guid id { get; set; }
+        public string AdabiyTil { get; set; }
+        public string Transliteratsiya { get; set; }
+        public string Transkripsiya { get; set; }
+        public string Turkum { get; set; }
+        public string ShevaIzohi { get; set; }
+        public string Misollar { get; set; }
+        public string Sinonim { get; set; }
+        public string Omonim { get; set; }
+        public string Antonim { get; set; }
+        public string OzlashganQatlam { get; set; }
+        public string RusTilida { get; set; }
+        public string IngilizTilida { get; set; }
+    }
+}
